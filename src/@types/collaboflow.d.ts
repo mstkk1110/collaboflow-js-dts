6a3410dd8899127a3a66b4b5648ecf505e28d8fd
mstkk1110 <117853950+mstkk1110@users.noreply.github.com>
2022-12-11 20:26:04 +0900

    feat: add API response.

--- src/@types/collaboflow.d.ts
+++ src/@types/collaboflow.d.ts
@@ -1,3 +1,37 @@
 declare namespace CollaboflowStatic {
   type TkeyValue = { [key: string]: any };
+
+  interface ApiResponse {
+    /**
+     * Collaboflow REST APIの応答HTTPステータスコードを取得します。
+     * @type {number}
+     * @memberof iApiResponse
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     */
+    readonly status: number;
+
+    /**
+     * Collaboflow REST APIの応答HTTPステータステキストを取得します。
+     * @type {number}
+     * @memberof iApiResponse
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     */
+    readonly status_text: string;
+
+    /**
+     * Collaboflow REST APIの応答ヘッダーを取得します。
+     * @type {number}
+     * @memberof iApiResponse
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     */
+    readonly headers: TkeyValue;
+
+    /**
+     * Collaboflow REST APIの応答内容を取得します。
+     * @type {number}
+     * @memberof iApiResponse
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     */
+    readonly body: TkeyValue;
+  }
 }
