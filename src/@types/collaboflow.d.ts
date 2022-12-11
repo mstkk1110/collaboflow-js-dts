4060b6d68573deb0c23c37cfe4adb57c7e557a90
mstkk1110 <117853950+mstkk1110@users.noreply.github.com>
2022-12-11 20:27:09 +0900

    feat: add API functions.

--- src/@types/collaboflow.d.ts
+++ src/@types/collaboflow.d.ts
@@ -34,4 +34,50 @@ declare namespace CollaboflowStatic {
      */
     readonly body: TkeyValue;
   }
+
+  interface Api {
+    /**
+     * Collaboflow REST API へ getリクエストを実行します。
+     * @param {string} path Collaboflow REST API のパス
+     * @param {TkeyValue} parameters リクエストパラメーター
+     * @returns {Promise<ApiResponse>} Promise
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     * @example collaboflow.api.get('/v1/documents/1', {'app_cd': 1})
+     * @memberof IApi
+     */
+    get(path: string, parameters: TkeyValue): Promise<ApiResponse>;
+
+    /**
+     * Collaboflow REST API へ postリクエストを実行します。
+     * @param {string} path Collaboflow REST API のパス
+     * @param {(TkeyValue | FormData)} parameters リクエストパラメーター
+     * @returns {Promise<ApiResponse>} Promise
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     * @example collaboflow.api.post('collaboflow rest api path', {'key': value})
+     * @memberof IApi
+     */
+    post(path: string, parameters: TkeyValue | FormData): Promise<ApiResponse>;
+
+    /**
+     * Collaboflow REST API へ putリクエストを実行します。
+     * @param {string} path Collaboflow REST API のパス
+     * @param {TkeyValue} parameters リクエストパラメーター
+     * @returns {Promise<ApiResponse>} Promise
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     * @example collaboflow.api.put('collaboflow rest api path', {'key': value})
+     * @memberof IApi
+     */
+    put(path: string, parameters: TkeyValue): Promise<ApiResponse>;
+
+    /**
+     * Collaboflow REST API へ deleteリクエストを実行します。
+     * @param {string} path Collaboflow REST API のパス
+     * @param {TkeyValue} parameters リクエストパラメーター
+     * @returns {Promise<ApiResponse>} Promise
+     * @see [collaboflow REST API](http://docs.collaboflow.com/api-docs/)
+     * @example collaboflow.api.delete('collaboflow rest api path', {'key': value})
+     * @memberof IApi
+     */
+    delete(path: string, parameters: TkeyValue): Promise<ApiResponse>;
+  }
 }
